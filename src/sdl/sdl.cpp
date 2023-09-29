3e3ab30c520b21b261199de964f868d9062d3d1f
Nomi <nomiwen@outlook.de>
2023-09-29 05:46:03 +0200

    Add SDL2 subproject

---
+++ src/sdl/sdl.cpp
@@ -0,0 +1,86 @@
+#include <cstdio>
+#include <cstdlib>
+#include <video/video.h>
+#include "sdl/sdl.h"
+
+#include <SDL.h>
+
+namespace SDL
+{
+
+using Video::DISPLAY_HEIGHT;
+using Video::DISPLAY_WIDTH;
+
+struct Screen
+{
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+	SDL_Texture* texture;
+};
+
+static Screen screen;
+
+static bool isInitialized = false;
+
+void initialize()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+		printf("Failed to initialize SDL2: %s\n", SDL_GetError());
+
+		exit(0);
+	}
+
+	//Try synchronizing drawing to VBLANK
+	SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+
+    //Set up SDL screen
+	SDL_CreateWindowAndRenderer(DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, &screen.window, &screen.renderer);
+    SDL_SetWindowTitle(screen.window, "Rupi");
+	SDL_SetWindowSize(screen.window, 2 * DISPLAY_WIDTH, 2 * DISPLAY_HEIGHT);
+	SDL_SetWindowResizable(screen.window, SDL_FALSE);
+	SDL_RenderSetLogicalSize(screen.renderer, 2 * DISPLAY_WIDTH, 2 * DISPLAY_HEIGHT);
+
+	screen.texture = SDL_CreateTexture(screen.renderer, SDL_PIXELFORMAT_ARGB1555, SDL_TEXTUREACCESS_STREAMING, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+    isInitialized = true;
+}
+
+void shutdown() {
+    //Destroy window, then kill SDL2
+    SDL_DestroyTexture(screen.texture);
+	SDL_DestroyRenderer(screen.renderer);
+	SDL_DestroyWindow(screen.window);
+
+    SDL_Quit();
+
+    isInitialized = false;
+}
+
+void update(uint16_t* display_output)
+{
+    if (!isInitialized)
+    {
+        return;
+    }
+
+    //Poll events
+    SDL_Event e;
+    while (SDL_PollEvent(&e))
+    {
+        switch (e.type)
+        {
+        case SDL_QUIT:
+            exit(0);
+        default:
+            break;
+        }
+    }
+
+    // Draw screen
+    SDL_UpdateTexture(screen.texture, NULL, display_output, sizeof(uint16_t) * DISPLAY_WIDTH);
+    SDL_RenderCopy(screen.renderer, screen.texture, NULL, NULL);
+    SDL_RenderPresent(screen.renderer);
+}
+
+}
